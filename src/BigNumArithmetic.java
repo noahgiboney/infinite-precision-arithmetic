b2da5a2c31357bdf4a9bc96e643a92f5e9e0f07c
noahgibony <noahgiboney@Noahs-MacBook-Air.local>
2023-10-04 23:33:59 -0700

    logic testing

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -7,12 +7,19 @@ public class BigNumArithmetic {
      * @param args Command line arguments. Should have exactly one argument: a file name.
      */
     public static void main(String[] args) {
-        if (args.length != 1) {
-            throw new IllegalArgumentException(
-                    "Expected exactly 1 argument: a file name.");
-        }
-        String filePath = args[0];
-        FileProcessor.processFile(filePath);
+//        if (args.length != 1) {
+//            throw new IllegalArgumentException(
+//                    "Expected exactly 1 argument: a file name.");
+//        }
+//        String filePath = args[0];
+//        FileProcessor.processFile(filePath);
+
+        //use this logic
+        int x = 112359;
+        System.out.println(x % 10);
+        System.out.println(x % 100 / 10);
+        System.out.println(x % 1000 / 100);
+        System.out.println(x % 10000 / 1000);
     }
 }
 
