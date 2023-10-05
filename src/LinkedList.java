d067b4bf017940e2383a726e2ae5bc33619c7a30
noahgiboney <noahgiboney@gmail.com>
2023-10-04 22:54:35 -0700

    worked on linked list

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -26,4 +26,34 @@ public class LinkedList {
         }
         return list;
     }
+
+    public LinkedList remove(LinkedList list, int value){
+
+        //find previous node of the node to be deleted
+        //change the next of the previous node
+        //free memory
+        Node temp = list.head;
+        Node previous = null;
+
+        if(temp != null && temp.value == value){ //if head ahs the value to delete assign next node as the head
+            list.head = temp.next;
+            return list;
+        }
+
+        while(temp != null && temp.value != value){
+            previous = temp;
+            temp = temp.next;
+        }
+        if(temp == null){
+            return list;
+        }
+
+        previous.next = temp.next;
+        return list;
+    }
+
+    public String toString(){
+
+        return null;
+    }
 }
