492ce31638b46d1e58c943ee3512b292551f8bcf
noahgiboney <noahgiboney@gmail.com>
2023-10-07 16:59:26 -0700

    worked on addition class

--- src/Addition.java
+++ src/Addition.java
@@ -1,25 +1,53 @@
 public class Addition {
     private String operator;
-    private int x;
-    private int y;
-    private LinkedList listX;
+    private String x;
+    private String y;
+    private LinkedList list;
     private LinkedList listY;
 
-    public Addition(String operator, int x, int y ){
+    public Addition(String operator, String x, String y ){
         this.operator = operator;
-        this.x = Integer.parseInt(Integer.toString(x,10));
-        this.y = Integer.parseInt(Integer.toString(y,10));
+
+        StringBuilder bufferX = new StringBuilder(x);
+        StringBuilder bufferY = new StringBuilder(y);
+
+        int indexX = 0;
+        while(bufferX.charAt(indexX) == '0'){
+            bufferX.deleteCharAt(indexX);
+        }
+        int indexY = 0;
+        while(bufferY.charAt(indexY) == '0'){
+            bufferY.deleteCharAt(indexY);
+        }
+        this.x = bufferX.toString();
+        this.y = bufferY.toString();
+    }
+
+    public LinkedList fillList(String value){
+        list = new LinkedList();
+
+        for(int i = 0; i < value.length(); i++){
+            list.insertNode(value.charAt(i) - '0');
+        }
+        return list;
+    }
+
+    public LinkedList doAddition(LinkedList listX, LinkedList listY){
+        LinkedList result = new LinkedList();
+
+
+        return result;
     }
 
     public String getOperator() {
         return operator;
     }
 
-    public int getX() {
+    public String getX() {
         return x;
     }
 
-    public int getY() {
+    public String getY() {
         return y;
     }
 }
--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -15,13 +15,13 @@ public class FileProcessor {
         try (Scanner scan = new Scanner(infile)) {
             while(scan.hasNext()){
 
-                    int x = scan.nextInt();
+                    String x = scan.next();
                     String operator = scan.next();
-                    int y = scan.nextInt();
+                    String y = scan.next();
 
                     if(Objects.equals(operator, "+")){
                         Addition add = new Addition(operator, x, y);
-
+                        add.doAddition(add.fillList(add.getX()), add.fillList(add.getY()));
 
                         System.out.println(add.getX() + " " + add.getOperator() + " " + add.getY() + " = ");
                     }
