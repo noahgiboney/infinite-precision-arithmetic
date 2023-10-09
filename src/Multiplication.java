ee6bb1422504da6e0f351dc5909a80535f7e3f32
noahgiboney <noahgiboney@gmail.com>
2023-10-08 23:36:55 -0700

    1 digit multiplication working

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -6,43 +6,18 @@ public class Multiplication extends Operation{
 
     @Override
     public LinkedList doOperation(LinkedList listX, LinkedList listY) {
-
-        LinkedList resultList = new LinkedList(); // list to store running total of adding multiplication result
+        LinkedList resultList = new LinkedList();
         int zeroCounter = 0;
 
         while(listY.getHead() != null){
-            LinkedList temp = new LinkedList();
-            LinkedList copy = listX;
-
-            while(copy.getHead() != null){
-
-                int result = listY.getHead().getValue() * listX.getHead().getValue();
+            LinkedList.Node temp = listX.getHead();
+            while(temp != null){
+                int result = listY.getHead().getValue() * temp.getValue();
                 if(result <= 9){
-                    temp.insertNode(result);
+                    resultList.insertNode(result);
                 }
-                else{
-                    temp.insertNode(result % 10);
-                    if(listX.getHead().getNext() == null){
-                        temp.insertNode(result / 10); //if the head of list x does not point to anything, then we can insert into the temp list
-                    }
-                    else{
-                        listX.getHead().getNext().carry(result / 10  ); //if there is another node after the head we have to carry the tens place value to the next node
-                    }
-                }
-                copy.removeHead();
+                temp = temp.getNext();
             }
-            for(int i = 0; i < zeroCounter; i++){
-                temp.insertNode(0);
-            }
-
-            if(resultList.getHead() == null){
-                resultList = temp;
-            }
-            else {
-                Addition add = new Addition();
-                resultList = add.doOperation(resultList, temp);
-            }
-            zeroCounter++;
             listY.removeHead();
         }
         return resultList;
