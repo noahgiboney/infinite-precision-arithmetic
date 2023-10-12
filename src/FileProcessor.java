bca451f4e80867ed3b9168fafe1285ba270fc2b3
noahgibony <noahgiboney@Noahs-MacBook-Air.local>
2023-10-11 20:37:47 -0700

    work

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -16,35 +16,7 @@ public class FileProcessor {
 
         try (Scanner scan = new Scanner(infile)) {
             while(scan.hasNext()){
-                String currentLine = scan.nextLine();
-
-                String[] items = currentLine.split("\\s+"); //split items with space
-
-                if (items.length != 3) {
-                    continue;
-                }
-
-                String x = items[0];
-                String operator = items[1];
-                String y = items[2];
-
-                if (!(isValidOperand(x)) || !(isValidOperator(operator)) || !(isValidOperand(y))) {
-                    continue;
-                }
-
-                if(Objects.equals(operator, "+")){
-                    Addition add = new Addition(x,y);
-                    LinkedList result = add.doOperation(add.fillList(add.getX()), add.fillList(add.getY()));
-                    System.out.println(add.getX() + " + " + add.getY() + " = " + result.toString());
-                }
-                if(Objects.equals(operator, "*")){
-                    Multiplication multiply = new Multiplication(x,y);
-                    LinkedList result = multiply.doOperation(multiply.fillList(multiply.getX()), multiply.fillList(multiply.getY()));
-                    System.out.println(multiply.getX() + " * " + multiply.getY() + " = " + result.toString());
-                }
-                if(Objects.equals(operator, "^")){
-                    System.out.println();
-                }
+                
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + infile.getPath());
