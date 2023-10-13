aeca8041a66ff724911a297a3115bc229c8bbe1b
noahgiboney <noahgiboney@gmail.com>
2023-10-12 20:57:32 -0700

    1 digit multiplcation working

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -7,44 +7,27 @@ public class Multiplication extends Operation{
     @Override
     public LinkedList doOperation(LinkedList listX, LinkedList listY) {
         LinkedList resultList = new LinkedList();
+        LinkedList current = new LinkedList();
         int zeroCounter = 0;
-        LinkedList temp;
 
         while(listY.getHead() != null){
-            temp = listX;
-            LinkedList current = new LinkedList();
-
-            while(temp.getHead() != null){
-                int result = listX.getHead().getValue() * temp.getHead().getValue();
-                if(result <= 9){
-                    current.insertNode(result);
-                }
-                else{
-                    current.insertNode(result % 10);
-                    if (temp.getHead().getNext() == null){
-                        current.insertNode(result / 10);
-                    }
-                    else{
-                        temp.getHead().getNext().carry(result / 10);
-                    }
-                }
-                temp.removeHead();
-            }
-
-            for(int i = 0; i < zeroCounter; i++){
-                current.insertNode(0);
-            }
-
-            if(resultList.getHead() == null){
-                resultList = current;
-            }
-            else {
-                Addition add = new Addition();
-                resultList = add.doOperation(resultList,current);
+            LinkedList temp = listX; //result temp list
+            int result = temp.getHead().getValue() * listY.getHead().getValue();
+            if(result <= 9){
+                current.insertNode(result);
             }
             zeroCounter++;
             listY.removeHead();
         }
+        
+        for(int i = 1; i < zeroCounter; i++){
+            current.insertNode(0);
+        }
+
+        if(resultList.getHead() == null){
+            resultList = current;
+        }
+        
         return resultList;
     }
 
